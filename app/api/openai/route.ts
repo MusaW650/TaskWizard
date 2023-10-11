898ac904357f55ac290be53955c963176c099b01
Musa Waseem <musaw650@gmail.com>
2023-10-11 13:11:54 -0400

    adding input fields for task generation

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -21,21 +21,43 @@ export async function POST(req: Request, res: Response) {
     messages: [
       {
         role: "system",
-        content: "You are a productivity wizard who is able to group tasks in a visually appealing organized way"
-        + "You are known to be the one person to go to help someone plan their day with their tasks"
-        + "You address people as motivated individuals. You always reply in an epic way"
-        + "Each main goal is numbered with HTML checkboxes"   
-        + "You go straight to the point, your replies are a list format of the main tasks themselves with subgoals for each." 
-        + "There should not be more than 3 subtasks for each goal"
-        + "You MUST render the HTML for the checkboxes as they are being displayed, not after the list!"
-   
-         
+        content: `
+TASK: You are a productivity wizard who is able to group tasks in a visually appealing organized way.
+You are known to be the one person to go to help someone plan their day with their tasks
+You address people as motivated individuals. You always reply in an epic way
+Each main goal is numbered with HTML checkboxes   
+You go straight to the point, your replies are a list format of the main tasks themselves with subgoals for each.
+There should not be more than 3 subtasks for each goal
+----------
+OUTPUT FORMAT:
+<input type="checkbox" id="task1" name="task1"> <label> --TASK HERE--</label><br>
+-subtask1 with time estimate--
+-subtask2 with time estimate--
+-subtask3 with time estimate--
+<input type="checkbox" id="task2" name="task2"><label> --TASK HERE-- </label><br>
+-subtask1 with time estimate--
+-subtask2 with time estimate--
+-subtask3 with time estimate--
+<input type="checkbox" id="task3" name="task3"><label> --TASK HERE-- </label><br>        
+-subtask1 with time estimate--
+-subtask2 with time estimate--
+-subtask3 with time estimate--
+`
+        
         },
       ...messages,
     ],
     stream: true,
     temperature: 0.5,
   });
+
+  // "You are a productivity wizard who is able to group tasks in a visually appealing organized way"
+  //       + "You are known to be the one person to go to help someone plan their day with their tasks"
+  //       + "You address people as motivated individuals. You always reply in an epic way"
+  //       + "Each main goal is numbered with HTML checkboxes"   
+  //       + "You go straight to the point, your replies are a list format of the main tasks themselves with subgoals for each." 
+  //       + "There should not be more than 3 subtasks for each goal"
+  //       + "You MUST render the HTML for the checkboxes as they are being displayed, not after the list!"
 
   // const messagesFromAPI = response.choices[0].message.content;
 
--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,138 +1,65 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useChat } from "ai/react";
-import { useRef } from "react";
-import Image from "next/image";
-import ReactDOM from "react-dom/client";
-import { AudioRecorder } from "react-audio-voice-recorder";
-import { render } from "react-dom";
-
-type ChatMessage = {
-  id: string;
-  role: string;
-  content: string;
-};
-
-type Task = {
-  text: string;
-  completed: boolean;
-};
+import React, { useState, FormEvent } from "react";
+import { useChat, Message } from "ai/react";
 
 const Chat = () => {
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [tasks, setTasks] = useState<Task[]>([]); // Initialize tasks as an empty array
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const [goal, setGoal] = useState("");
+  const [time, setTime] = useState("");
+  const [importance, setImportance] = useState("");
+  const { messages, input, handleInputChange, handleSubmit, append } = useChat({
     api: "/api/openai",
   });
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    handleSubmit(event);
-  };
+    // Concatenate the three values into a single input
+    const concatenatedInput = `Goal: ${goal}, Time: ${time}, Importance: ${importance}`;
+    append({
+      content: concatenatedInput,
+      role: 'user'
+    });
 
-  const userColors = {
-    user: "#00c0ff",
-    assistant: "#e02aff",
-    function: "#fff",
-    system: "#fff",
-  };
-
-  // const addAudioElement = (blob: Blob) => {
-  //   const url = URL.createObjectURL(blob);
-  //   const audio = document.createElement("audio");
-  //   audio.src = url;
-  //   audio.controls = true;
-  //   document.body.appendChild(audio);
-  // };
-
-  // ReactDOM.createRoot(document.getElementById("root")).render(
-  //   <React.StrictMode>
-  //     <AudioRecorder
-  //       onRecordingComplete={addAudioElement}
-  //       audioTrackConstraints={{
-  //         noiseSuppression: true,
-  //         echoCancellation: true,
-  //       }}
-  //       downloadOnSavePress={true}
-  //       downloadFileExtension="webm"
-  //     />
-  //   </React.StrictMode>
-  // );
-
-  const renderResponse = () => {
-    return (
-      <div className="response">
-        {messages.length > 0
-          ? messages.map((m) => (
-              <div key={m.id} className="chat-line">
-                <span style={{ color: userColors[m.role] }}>
-                  {m.role === "user" ? "User: " : "⚡️Task Wizard: "}
-                </span>
-                <div dangerouslySetInnerHTML={{ __html: m.content }} />
-            
-                {/* {m.content} */}
-              </div>
-            ))
-          : error}
-      </div>
-    );
-  };
-
-  const toggleTaskStatus = (index: number) => {
-    // Specify the type here
-    const updatedTasks = [...tasks];
-    updatedTasks[index].completed = !updatedTasks[index].completed;
-    setTasks(updatedTasks);
-  };
-
-  const renderTasks = () => {
-    return (
-      <div className="response">
-        {messages.length > 0
-          ? messages.map((m, index) => (
-              <div key={m.id} className="chat-line">
-                <span style={{ color: userColors[m.role] }}>
-                  {m.role === "user" ? "User: " : "⚡️Task Wizard: "}
-                </span>
-                {m.content.split("\n").map((line, lineIndex) => (
-                  <div key={lineIndex}>
-                    {line.startsWith(`${lineIndex + 1}.`) ? (
-                      <>
-                        {line}
-                        <button onClick={() => toggleTaskStatus(lineIndex)}>
-                          Complete
-                        </button>
-                      </>
-                    ) : (
-                      line
-                    )}
-                  </div>
-                ))}
-              </div>
-            ))
-          : error}
-      </div>
-    );
+    // Reset the individual fields
+    setGoal("");
+    setTime("");
+    setImportance("");
   };
 
   return (
-    <>
-      {renderResponse()}
-      <form onSubmit={handleSubmit} className="mainForm">
-        <input
-          name="input-field"
-          placeholder="Say anything"
-          onChange={handleInputChange}
-          value={input}
-        />
-        <button type="submit" className="mainButton">
-          SUBMIT
-        </button>
+    <div className="chat-container"> {/* Surrounding container for centering */}
+      <form onSubmit={onSubmit} className="mainForm">
+        <div className="button-container">
+          <input
+            id="goal"
+            type="text"
+            placeholder="Goal"
+            value={goal}
+            onChange={(e) => setGoal(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Time to Accomplish"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Importance"
+            value={importance}
+            onChange={(e) => setImportance(e.target.value)}
+          />
+          <button type="submit" className="mainButton">
+            SUBMIT
+          </button>
+        </div>
       </form>
-    </>
+
+      {messages.map((m: Message) => (
+        <div key={m.id} className="chat-line">
+          <div dangerouslySetInnerHTML={{ __html: m.content }} />
+        </div>
+      ))}
+    </div>
   );
 };
 
